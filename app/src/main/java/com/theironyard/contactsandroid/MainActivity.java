7ec3e0bde6811872ed4ae81d6a4900a4300eb228
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-21 16:40:45 -0400

    commit for submission

--- app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
@@ -2,12 +2,55 @@ package com.theironyard.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
+
+    ArrayAdapter<String> items;
+
+    ListView listView;
+    EditText name;
+    EditText phone;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
+        addButton =  (Button) findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(this);
+        listView.setOnItemLongClickListener(this);
+
+        items = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(items);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = name.getText().toString();
+        String text2 = phone.getText().toString();
+        items.add(text + " (" + text2 + ")");
+
+        name.setText("");
+        phone.setText("");
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        String itemToRemove = items.getItem(position);
+        items.remove(itemToRemove);
+        return true;
     }
 }
